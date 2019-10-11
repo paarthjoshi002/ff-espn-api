785904f5d394fdb840c6099413b58e8ab021f2be
paarthjoshi002 <44733838+paarthjoshi002@users.noreply.github.com>
2019-10-10 23:56:31 -0500

    very basic start to application

---
+++ ff_espn_api/runner.py
@@ -0,0 +1,11 @@
+### Initial Start
+from ff_espn_api import League
+
+league_id = 1180821
+year = 2018
+username = 'parth_joshi@hotmail.com'
+password = 'Stevemcnair00'
+league = League(league_id, year, username, password)
+
+#for team in league.teams:
+#    print(team.team_name)
