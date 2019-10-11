e96da9b3aff6a0b9b330ab8c76d148742f15de53
Paarth Joshi <rr828738@fincoad.com>
2019-10-11 01:54:49 -0500

    rankings

--- business_logic/league_data.py
+++ business_logic/league_data.py
@@ -1,5 +1,8 @@
+from ff_espn_api import League
+
 class League_Data():
 
     @staticmethod
-    def get_teams(league):
-        return 8
+    def get_points_allowed_ranking(league):
+        best_defense = sorted(league.teams, key=lambda x: x.points_against, reverse=True)
+        return best_defense
--- ff_espn_api/runner.py
+++ ff_espn_api/runner.py
@@ -1,6 +1,7 @@
 ### Initial Start
 from ff_espn_api import League
 from ff_espn_api import Team
+from business_logic.league_data import League_Data
 
 def main():
     league_id = 246927
@@ -9,8 +10,9 @@ def main():
     password = 'vemcnair002'
     league = League(league_id, year, username, password)
 
-    for team in league.teams:
-        print(team.team_name)
+    best_defense = League_Data.get_points_allowed_ranking(league)
+    for t in best_defense:
+        print(t.team_name + " " + t.points_against)
 
 if __name__ == "__main__":
     main()
